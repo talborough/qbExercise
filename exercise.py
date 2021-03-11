efa101559bc6caaf5aa87a47a8d2b07d367c1f90
Tom Alborough <tom.alborough@gmail.com>
2021-03-10 20:29:09 -0500

    Fix tab stops

--- exercise.py
+++ exercise.py
@@ -9,40 +9,40 @@ from handleInts import HandleInts
 #
 def testIt (theList, expected1, expected2, expectException):
 
-	print ("")
-	print (f"Testing with list = {theList}")
+    print ("")
+    print (f"Testing with list = {theList}")
 
-	# Call the method and note any exception
-	caughtException = False
-	try:
-		(actual1, actual2) = HandleInts.returnTwoMaxFromList(theList)
-	except ValueError:
-		caughtException = True
+    # Call the method and note any exception
+    caughtException = False
+    try:
+        (actual1, actual2) = HandleInts.returnTwoMaxFromList(theList)
+    except ValueError:
+        caughtException = True
 
-	# If the exception was not as expected FAIL
-	if expectException != caughtException:
-		print (f"FAILURE - Caught exception = {caughtException}; Expect exception = {expectException}")
-		exit (1)
+    # If the exception was not as expected FAIL
+    if expectException != caughtException:
+        print (f"FAILURE - Caught exception = {caughtException}; Expect exception = {expectException}")
+        exit (1)
 
-	# Any exception leaves no actuals so exit PASSED here
-	if caughtException:
-		print ("Caught exception as expected - test PASSED")
-		return (None, None)
+    # Any exception leaves no actuals so exit PASSED here
+    if caughtException:
+        print ("Caught exception as expected - test PASSED")
+        return (None, None)
 
-	print (f"Expected #1 & #2 = {expected1}, {expected2}")
-	print (f"Actual #1 & #2 = {actual1}, {actual2}")
+    print (f"Expected #1 & #2 = {expected1}, {expected2}")
+    print (f"Actual #1 & #2 = {actual1}, {actual2}")
 
-	# Check the first set of values
-	if actual1 != expected1:
-		print (f"FAILURE; actual1 = {actual1}; expected1 = {expected1}")
-		exit (1)
+    # Check the first set of values
+    if actual1 != expected1:
+        print (f"FAILURE; actual1 = {actual1}; expected1 = {expected1}")
+        exit (1)
 
-	# Check the secondt set of values
-	if actual2 != expected2:
-		print (f"FAILURE; actual2 = {actual2}; expected2 = {expected2}")
-		exit (1)
+    # Check the secondt set of values
+    if actual2 != expected2:
+        print (f"FAILURE; actual2 = {actual2}; expected2 = {expected2}")
+        exit (1)
 
-	print ("Test PASSED")
+    print ("Test PASSED")
 
 #
 # The integer lists to test with
@@ -63,4 +63,3 @@ testIt (listTwo, 114, 97,  False)
 testIt (listThree, 2, 1,  False)
 testIt (listFour, 5, None,  False)
 testIt (listFive, None, None, True)
-
--- handleInts.py
+++ handleInts.py
@@ -3,26 +3,26 @@
 #
 class HandleInts:
 
-	#
-	# A static method to return, as a list of 2 elements, the two largest numbers from a passed-in list.
-	# If there is no "second largest" number, None is returned.
-	# If there are not at least 2 numbers in the list, a ValueError exception is raised and the two returned values are undefined
-	#
-	def returnTwoMaxFromList (listOfIntegers):
+    #
+    # A static method to return, as a list of 2 elements, the two largest numbers from a passed-in list.
+    # If there is no "second largest" number, None is returned.
+    # If there are not at least 2 numbers in the list, a ValueError exception is raised and the two returned values are undefined
+    #
+    def returnTwoMaxFromList (listOfIntegers):
 
-		if len(listOfIntegers) < 2:
-			raise (ValueError("Must provide a list of 2 or more elements"))
+        if len(listOfIntegers) < 2:
+            raise (ValueError("Must provide a list of 2 or more elements"))
 
-		listOfIntegers.sort(reverse = True)
+        listOfIntegers.sort(reverse = True)
 
-		# THe largest is always here
-		firstLargest = listOfIntegers[0]
+        # THe largest is always here
+        firstLargest = listOfIntegers[0]
 
-		# Skip possible duplicates to find (possible) next largest
-		nextLargest = None
-		for anInteger in listOfIntegers:
-			if anInteger < firstLargest:
-				nextLargest = anInteger
-				break
+        # Skip possible duplicates to find (possible) next largest
+        nextLargest = None
+        for anInteger in listOfIntegers:
+            if anInteger < firstLargest:
+                nextLargest = anInteger
+                break
 
-		return (firstLargest, nextLargest)
+        return (firstLargest, nextLargest)
